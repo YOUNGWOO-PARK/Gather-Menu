425ea4d965c6b452667a394a6bc961f911a43bca
여우비 <77572677+YOUNGWOO-PARK@users.noreply.github.com>
2021-08-14 22:32:23 +0900

    Update User.java

--- src/User.java
+++ src/User.java
@@ -52,7 +52,7 @@ public class User implements Runnable{
     	}
     }
     
-    public void print (String send, int Additional) throws Exception{
+    public void print (String send, int Additional) throws Exception{ //사용자에게 화면 출력
     	StringBuffer msg = new StringBuffer();
     	msg.append("HTTP/1.1 200 OK").append("\r\n");
 		msg.append("Host: localhost").append("\r\n");
@@ -66,39 +66,42 @@ public class User implements Runnable{
 		writer.write(msg.toString());
     }
     
-    public void order () throws Exception {
+    public void order () throws Exception { //사용자의 입력을 처리
     	reader = new BufferedReader(new InputStreamReader(
     			client.getInputStream()));
     	String choice = reader.readLine().split("[ ]")[1];
         int foodNumber =Integer.valueOf(choice.substring(1)) - 1;
         String status = "";
         
-        if (foodNumber <= -1 && ipManagement.get(ip) != -1) {
+        if (foodNumber <= -1 && ipManagement.get(ip) != -1) {// 정상적인 메뉴취소
         	memory.menu.get(ipManagement.get(ip)).countDown();
     		ipManagement.put(ip, -1);
     		status = "주문취소";
-        } else if (foodNumber <= -1 && ipManagement.get(ip) == -1) {
-        	ipManagement.put(ip, -1);
-        } else {
+        } else if ((foodNumber >= memory.menu.size() ||foodNumber <= -1)
+        		&& ipManagement.get(ip) == -1) {
+        	status = "다시주문해주세요";
+        } else {//주문 정상 처리
         	if (ipManagement.get(ip) == -1) {
         		ipManagement.put(ip, foodNumber);
         		memory.menu.get(foodNumber).countUp();
-        		System.out.println(memory.menu.get(foodNumber).name);
-        		System.out.println("new order");
         		status = "주문완료";
-        	}else {
+        		
+        	}else if (foodNumber < memory.menu.size()) {
         		memory.menu.get(ipManagement.get(ip)).countDown();
         		memory.menu.get(foodNumber).countUp();
-        		System.out.println(memory.menu.get(ipManagement.get(ip)).name + "취소");
-        		System.out.println(memory.menu.get(foodNumber).name + "변경");
         		ipManagement.put(ip, foodNumber);
         		status = "주문변경";
         	}
         }
-        print(htmlDoc + status, length + 4);
+        
+        print(htmlDoc + status, length + status.length());
+        
+        if (foodNumber >= 0 && foodNumber < memory.menu.size()) {
+        	System.out.println(memory.menu.get(foodNumber).name + status);
+        }
     }
     
-    public String getClientIp() {
+    public String getClientIp() { //사용자의 IP를 얻어옴
     	String originalIp = client.toString(); 
     	
     	return originalIp.substring(
